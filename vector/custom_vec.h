9fe30cefdc85353b62efb9d01eddf899780aca52
arsamnaqvi <arsamhassan1999@gmail.com>
2022-04-15 13:50:35 -0500

    vector definitions

--- vector/custom_vec.h
+++ vector/custom_vec.h
@@ -1,5 +1,23 @@
 
+namespace custom{
+
 template <typename T>
-class arsam_vector {
-    private 
-};
+class vec {
+    protected:
+        unsigned int _size;
+        unsigned int _capacity;
+        T* _data;
+
+    public:
+        vec();
+        vec(unsigned int n);
+        vec(unsigned int n, T val);
+        ~vec();
+        vec operator= (const vec& x);
+        unsigned int size();
+        unsigned int capacity();
+        bool empty();
+        T& operator[] (const int& i);
+};//end class vec
+
+};// end namespace custom
