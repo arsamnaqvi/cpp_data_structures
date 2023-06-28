d48f1e614c6fbc20872ba1b2069f3d10daee0f41
arsamnaqvi <arsamhassan1999@gmail.com>
2023-06-28 18:42:17 -0500

    more stubs

--- vector/custom_vec.h
+++ vector/custom_vec.h
@@ -1,23 +1,46 @@
 
-namespace custom{
+namespace custom {
 
-template <typename T>
-class vec {
-    protected:
-        unsigned int _size;
-        unsigned int _capacity;
-        T* _data;
+template <typename T> class vector {
+protected:
+  unsigned int _size;
+  unsigned int _capacity;
+  T *_data;
 
-    public:
-        vec();
-        vec(unsigned int n);
-        vec(unsigned int n, T val);
-        ~vec();
-        vec operator= (const vec& x);
-        unsigned int size();
-        unsigned int capacity();
-        bool empty();
-        T& operator[] (const int& i);
-};//end class vec
+public:
+  class iterator {};
+  // constructors and destructors
+  vector();
+  vector(unsigned int n);
+  vector(unsigned int n, T val);
+  ~vector();
 
-};// end namespace custom
+  // element access
+  T &at(unsigned int pos);
+  T &operator[](const int &i);
+  T &front();
+  T &back();
+  T *data();
+
+  // iterators
+  iterator begin() noexcept;
+  iterator end() noexcept;
+  iterator rbegin() noexcept;
+  iterator rend() noexcept;
+
+  // capacity
+  bool empty();
+  unsigned int size();
+  unsigned int max_size();
+  void reserve(unsigned int new_cap);
+  unsigned int capacity();
+  void shrink_to_fit();
+
+  // modifiers
+  void clear();
+  iterator insert(const iterator pos);
+  // others
+  vector operator=(const vector &x);
+}; // end class vector
+
+}; // end namespace custom
